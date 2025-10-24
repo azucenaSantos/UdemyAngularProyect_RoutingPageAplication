74651bfdc2181fa6d9a5e35f021d49f55f7a7600
Azucena <azu.santos.ete@gmail.com>
2025-10-24 14:29:55 +0200

    Dynamic Route Parameters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
+  //Vamos a registrar más rutas, que es como será realmente el funcionamiento del enrutamiento
+  {
+    path: '', //dominio (sin /rutas varias)
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks', //dominio/tasks
     component: TasksComponent, //componente que se renderizará
     //Con solo esta configuracion si accecemos al dominio/tasks no se ve nada del TasksComponent
     //porque angular no sabe donde debe mostrarlo, y mejor asi para evitar problemas de que se sobreescriban
     //o desaparezcan componentes :
-    //Tenemos que decirle a angular dónde debe mostrar este componente TasksComponent (desde el app.component.html añadir un marcado 
+    //Tenemos que decirle a angular dónde debe mostrar este componente TasksComponent (desde el app.component.html añadir un marcado
     //de donde debe mostrar el componente)
   },
-  //Vamos a registrar más rutas, que es como será realmente el funcionamiento del enrutamiento
+  {
+    //Crearemos una ruta para poder acceder a las tasks de un usuario en concreto
+    //"Ruta dinámica"
+    path: 'users/:userId', //userId será un parámetro dinámico de la ruta
+    component: UserTasksComponent, //componente que se mostrará
+  },
 ];
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +8,34 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  private usersService = inject(UsersService);
+  private destroyRef = inject(DestroyRef);
+
+  //Tenemos que obtener el id de la url, para obtener el nombre del user y mostrarlo en el componente
+  //userId = input.required<string>(); //con esto ya obtenemos el id de la ruta!! MAGIA XD tiene
+  //que ser el mismo nombre que hemos puesto en el app.routes.ts en la path si no es el mismo no va xd
+
+  /*userName = computed(
+    () => this.usersService.users.find((u) => u.id == this.userId())?.name
+  );*/
+  //obtenemos el NAME de aquel usuario cuya id sea como la del userId
+
+  /***************************************/
+
+  //Otra forma de obtener el id de la ruta sin ser con input
+  private activatedRoute = inject(ActivatedRoute); //info sobre la ruta que está en la url en este momento
+  userName = '';
+
+  ngOnInit() {
+    console.log(this.activatedRoute);
+    const suscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) =>
+        (this.userName =
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''), //el paramMap nos permite hacer un get de la parte de la url que queramos
+      //en este caso userId que es lo que pusimos en la path del app.routes.ts
+    });
+    this.destroyRef.onDestroy(() => suscription.unsubscribe());
+  }
+}

009c4a09251312e73efb7381fb88e2980290da24
Azucena <azu.santos.ete@gmail.com>
2025-10-24 10:54:32 +0200

    One path configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,11 @@ export const routes: Routes = [
   {
     path: 'tasks', //dominio/tasks
     component: TasksComponent, //componente que se renderizará
+    //Con solo esta configuracion si accecemos al dominio/tasks no se ve nada del TasksComponent
+    //porque angular no sabe donde debe mostrarlo, y mejor asi para evitar problemas de que se sobreescriban
+    //o desaparezcan componentes :
+    //Tenemos que decirle a angular dónde debe mostrar este componente TasksComponent (desde el app.component.html añadir un marcado 
+    //de donde debe mostrar el componente)
   },
+  //Vamos a registrar más rutas, que es como será realmente el funcionamiento del enrutamiento
 ];

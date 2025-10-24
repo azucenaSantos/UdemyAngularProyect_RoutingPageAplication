1d1c2cd7d2e78c2f16cd182132e7999b08de1cb3
Azucena <azu.santos.ete@gmail.com>
2025-10-24 10:47:43 +0200

    Initial configuration and adding a first route

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { TasksComponent } from './app/tasks/tasks.component';
+import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(
+  AppComponent,
+  //     {
+  //   providers: [
+  //     // provideRouter([
+  //     //   {
+  //     //     //Con esta configuracion estamos registrando una ruta
+  //     //     path: 'tasks', //dominio/tasks
+  //     //     component: TasksComponent, //componente que se renderizará
+  //     //   },
+  //     //   //es posible que tengamos muchas rutas y seria costoso implementarlas aqui
+  //     //   //asi que creamos un app.routes.ts
+  //     // ]),
+
+  //     //para más simplificado,pasamos routes que está en el app.routes.ts
+  //     provideRouter(routes),
+  //   ],
+  // }
+
+  //INCLUSO PODEMOS SIMPLIFICAR MÁS ESTA CONFIGURACION CREANDO UN APP.CONFIG.TS
+  appConfig //pasamos solo el objeto que tiene el app.config.ts que a su vez tiene las path que tiene el app.routes.ts
+  //más estrcturado y más visible :D
+).catch((err) => console.error(err));

db299c5364528710beaee6bfec848b454612334e
Azucena <azu.santos.ete@gmail.com>
2025-10-27 17:20:55 +0100

    Nested Routes (rutas anidadas)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   //Vamos a registrar más rutas, que es como será realmente el funcionamiento del enrutamiento
@@ -23,5 +24,20 @@ export const routes: Routes = [
     //"Ruta dinámica"
     path: 'users/:userId', //userId será un parámetro dinámico de la ruta
     component: UserTasksComponent, //componente que se mostrará
+    //configuraremos rutas hijas, para acceder a las tasks de un user en concreto
+    children: [
+      {
+        path: 'tasks', //dominio/users/userId/tasks
+        component: TasksComponent,
+        //OJO! como es una clase hija tenemos que añadir un router-outlet en el componente padre porque el del app.component no es suficiente
+        //para poder acceder a una ruta huja de un componente!!
+        //en el user-tasks.component.html -> añadimos router-outlet donde queramos mostrar a nuestro TaskComponent al acceder a tasks de un user
+      },
+      {
+        //Con esta nueva ruta hija mostraremos el formulario de añadir una nueva task al acceder a tasks/new :D
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
